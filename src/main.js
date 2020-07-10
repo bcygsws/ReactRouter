dc39741cab82d7df91513b440452e8ffde892396
bcy <bcygsws@163.com>
2020-07-11 00:23:19 +0800

    React框架-路由2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 /* 这是js入口文件 */
 import React from 'react';
 import ReactDOM from 'react-dom';
+// 如果要使用路由模块，yarn add react-router-dom,导入路由模块，采用按需导入的方式
+import {HashRouter,Route,Link} from 'react-router-dom';
 // const l1 = React.createElement('li', null, 'one');//列表项不设置key，将提示警告:Warning: Each child in a list should have a unique "key" prop.
 // const l1 = React.createElement('li', { key: 1 }, 'one'); //列表项不设置key，将提示警告:Warning: Each child in a list should have a unique "key" prop.
 // const l2 = React.createElement('li', { key: 2 }, 'two');
